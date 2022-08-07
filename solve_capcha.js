f0a56cbc32b312eba8562e2c399726136792d816
Thai <tonytran.tm@gmail.com>
2022-08-07 18:32:48 +0700

    capMonster_key

--- solve_capcha.js
+++ solve_capcha.js
@@ -1,10 +1,12 @@
 const { default: axios } = require("axios");
 const capmonster = require("capmonster");
 
+const capMonster_key = '';
+
 const solveCaptcha = async ({ iframeLink, baseURL, pageURL, cookies }) => {
   const url = new URL(iframeLink);
   const siteKey = url.searchParams.get("sitekey");
-  const captcha = new capmonster(config.captcha.capMonster.key);
+  const captcha = new capmonster(capMonster_key);
   await captcha.decodeReCaptchaV2(pageURL, siteKey).then(async (res) => {
     if (res.data) {
       const params = new URLSearchParams();
